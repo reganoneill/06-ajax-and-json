dda39f6d25f543b6c4d846f7e5e4b3887526d04f
reganoneill <rkrotho@gmail.com>
2016-10-31 17:19:59 -0700

    loaded localStorage from database and setup conditionals to pull data from localStorage if needed

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -46,12 +46,24 @@ Article.fetchAll = function() {
     /* When our data is already in localStorage:
     1. We can process and load it,
     2. Then we can render the index page.  */
+
+    var cachedLocalStorage = JSON.parse(localStorage.blogArticles);
+    Article.loadAll(cachedLocalStorage);
+    articleView.renderIndexPage();
+    console.log('first condition is running');
   } else {
     /* Without our localStorage in memory, we need to:
     1. Retrieve our JSON file with $.getJSON
       1.a Load our json data
       1.b Store that data in localStorage so that we can skip the server call next time,
       1.c And then render the index page.*/
+//DONE!
+    $.getJSON('/data/blogArticles.json', function(data){
+      Article.loadAll(data);
+      localStorage.blogArticles=JSON.stringify(data);
+      articleView.renderIndexPage();
+      console.log('else is running');
+    });
   }
 };
 
@@ -68,3 +80,5 @@ Article.fetchAll = function() {
     } else {}
   }
 */
+
+// Article.fetchAll();
